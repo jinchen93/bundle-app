b75cc7061d44b253630f9a8008cb62d6dde95117
Jin Chen <jinchen93@gmail.com>
2016-12-22 22:14:17 -0800

    new production build, use express instead of pushstate server

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
-const server = require('pushstate-server');
+const express = require('express');
+const path = require('path');
 
-server.start({
-  port: 9000,
-  directory: './build'
-})
+const app = express();
+
+function requireHTTPS(req, res, next) {
+  if (!req.secure) {
+    //FYI this should work for local development as well
+    return res.redirect('https://' + req.get('host') + req.url);
+  }
+  next();
+}
+
+app.use(requireHTTPS);
+app.use(express.static('build'));
+
+app.get('*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(9000, console.log('Running on Port 9000'));
