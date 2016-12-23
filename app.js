a7baf1360023754f4788ab30cc6b394b66507fb5
Jin Chen <jinchen93@gmail.com>
2016-12-22 22:16:33 -0800

    remove https redirect, doesnt work on production build

--- app.js
+++ app.js
@@ -3,15 +3,6 @@ const path = require('path');
 
 const app = express();
 
-function requireHTTPS(req, res, next) {
-  if (!req.secure) {
-    //FYI this should work for local development as well
-    return res.redirect('https://' + req.get('host') + req.url);
-  }
-  next();
-}
-
-app.use(requireHTTPS);
 app.use(express.static('build'));
 
 app.get('*', function(req, res) {
