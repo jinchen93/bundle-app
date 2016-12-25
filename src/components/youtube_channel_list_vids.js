c04c299ecdf45afe6cef69058e67e27391d714b2
Jin Chen <jinchen93@gmail.com>
2016-12-24 17:38:40 -0800

    Remove channel header padding to fix the hover effect. Centered channel name using padding-left equal to hamburger width. Window sizes < col-md will now have 3 thumbnails per row.

--- src/components/youtube_channel_list_vids.js
+++ src/components/youtube_channel_list_vids.js
@@ -7,7 +7,7 @@ const YoutubeChannelListVids = ({channelVideos, onVideoSelect}) => {
       {
         channelVideos.map( video => { 
           return( 
-            <div className="col-md-2 col-xs-6 videos" key={video.etag}>
+            <div className="col-md-2 col-sm-4 col-xs-4 videos" key={video.etag}>
               <div className="video">
                 <img onClick={ () => onVideoSelect(video, video.snippet.resourceId.videoId) } 
                     className="thumbnail" src={video.snippet.thumbnails.medium.url} 
