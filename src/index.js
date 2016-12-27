51d19811ace1b571f39c5ec304651a27e58d943f
jin <jinchen93@gmail.com>
2016-12-27 08:03:25 -0800

    Add basic file structure for Redux implentation

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 import '../styles/style.css';
 
 ReactDOM.render(
