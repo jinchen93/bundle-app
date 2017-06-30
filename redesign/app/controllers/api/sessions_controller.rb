6598fb035588db96ff9e8db38b2c0b7df7801662
Jin Chen <jinchen93@gmail.com>
2017-06-30 09:49:17 -0700

    Add sessions controller and routes

---
+++ redesign/app/controllers/api/sessions_controller.rb
@@ -0,0 +1,30 @@
+class Api::SessionsController < ApplicationController
+  def create
+    @user = User.find_by_credentials(
+      user_params[:username],
+      user_params[:password]
+    )
+
+    if @user
+      login!(@user)
+      render 'api/users/show'
+    else
+      render json: ["Invalid credentials"], status: 422
+    end
+  end
+
+  def destroy
+    if current_user
+      logout!
+      render json: {}
+    else
+      render json: {}, status: 422
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
+end
--- redesign/config/routes.rb
+++ redesign/config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api, defaults: { format: :json }  do
     resources :users, only: [:create, :update]
+    resource :session, only: [:create, :destroy]
+
     resources :youtube_channels, only: [:index, :show, :create, :destroy]
   end
 
