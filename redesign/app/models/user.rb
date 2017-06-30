715a836e739a62c83e32d239ab52be55e873b08e
Jin Chen <jinchen93@gmail.com>
2017-06-30 09:47:43 -0700

    Add users model, routes, and controller

---
+++ redesign/app/models/user.rb
@@ -0,0 +1,43 @@
+class User < ApplicationRecord
+  after_initialize :ensure_session_token
+
+  validates :username, presence: true, uniqueness: { case_sensitive: false }
+  validates :session_token, presence: true, uniqueness: true
+  validates :password_digest, presence: true
+  validates :password, length: { minimum: 6, allow_nil: true }
+
+  attr_reader :password
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    return nil if user.nil?
+    user.is_password?(password) ? user : nil
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = encrypt_password(password)
+  end
+
+  def encrypt_password(password)
+    BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def generate_session_token
+    SecureRandom::urlsafe_base64(12)
+  end
+
+  def reset_session_token!
+    self.session_token = generate_session_token
+    self.save!
+    self.session_token
+  end
+
+  def ensure_session_token
+    self.session_token ||= generate_session_token
+  end
+end
