1a5d2368fdee84632963f1bd599725f01bb8b745
Jin Chen <jinchen93@gmail.com>
2017-07-02 10:42:53 -0700

    Add session api utils, session actions, and session reducer

--- redesign/client/entry.js
+++ redesign/client/entry.js
@@ -6,7 +6,18 @@ require("./styles/index.scss");
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
-  const store = configureStore();
+  let store;
+
+  if (window.currentUser) {
+    const preloadedState = {
+      session: { currentUser: window.currentUser },
+    };
+    store = configureStore(preloadedState);
+    delete window.currentUser;
+  } else {
+    store = configureStore();
+  }
+
 
   ReactDOM.render(<Root store={store} />, root);
 
