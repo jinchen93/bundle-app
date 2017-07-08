5865fecd89c00f238516202451fec3c8fa41e951
Jin Chen <jinchen93@gmail.com>
2017-07-08 11:16:03 -0700

    Add youtube, reddit, and twitch logo buttons

--- client/components/listing/ListingHeader.jsx
+++ client/components/listing/ListingHeader.jsx
@@ -1,5 +1,44 @@
 import React from "react";
 
-const ListingHeader = props => <div className="listing-header" />;
+const ListingHeader = () => {
+  const handleClick = e => {
+    e.preventDefault();
+    const target = e.target;
+    target.classList.add("active");
+    target.addEventListener("transitionend", () => {
+      console.log("end");
+      target.classList.remove("active");
+    });
+  };
+
+  return (
+    <div className="listing-header">
+      <span className="logo-wrapper">
+        <img
+          onClick={handleClick}
+          className="youtube-logo"
+          src="http://res.cloudinary.com/jinchen93/image/upload/v1499531225/Youtube-Logo_xoozbl.png"
+          alt="Youtube Logo"
+        />
+      </span>
+      <span className="logo-wrapper">
+        <img
+          onClick={handleClick}
+          className="reddit-logo"
+          src="http://res.cloudinary.com/jinchen93/image/upload/v1499531479/Reddit-Logo_tmylia.png"
+          alt="Reddit Logo"
+        />
+      </span>
+      <span className="logo-wrapper">
+        <img
+          onClick={handleClick}
+          className="twitch-logo"
+          src="http://res.cloudinary.com/jinchen93/image/upload/v1499531062/Twitch-Logo_bgbftx.png"
+          alt="Twitch Logo"
+        />
+      </span>
+    </div>
+  );
+};
 
 export default ListingHeader;
