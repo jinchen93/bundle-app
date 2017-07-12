e44a415d4e8727878c6ef11db615e35bc748029b
Jin Chen <jinchen93@gmail.com>
2017-07-11 17:02:28 -0700

    Add subreddit follow controller actions

--- app/controllers/api/subreddit_follows_controller.rb
+++ app/controllers/api/subreddit_follows_controller.rb
@@ -1,3 +1,40 @@
 class Api::SubredditFollowsController < ApplicationController
+  def create
+    subreddit_name = get_valid_name(subreddit_follow_params[:name])
+    @subreddit = Subreddit.find_by(name: subreddit_name)
 
+    if @subreddit
+      create_follow(@subreddit)
+    else
+      @subreddit = Subreddit.new({ name: subreddit_name })
+      if @subreddit.save
+        create_follow(@subreddit)
+      else
+        render json: @subreddit.errors.full_messages, status: 422
+      end
+    end
+  end
+
+  def get_valid_name(name)
+    return name[2..-1] if name[0..1] == "r/"
+    return name[3..-1] if name[1..2] == "r/"
+    name
+  end
+
+  def create_follow(subreddit)
+    follow = SubredditFollow.new({
+      subreddit_id: subreddit.id,
+      user_id: current_user.id
+    })
+    if follow.save
+      render :show
+    else
+      render json: follow.errors.full_messages, status: 422
+    end
+  end
+
+  private
+  def subreddit_follow_params
+    params.require(:subreddit_follow).permit(:name)
+  end
 end
