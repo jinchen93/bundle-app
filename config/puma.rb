c1f9a5f1e17ce2c304824b24a298a3694f8a88f6
Jin Chen <jinchen93@gmail.com>
2017-07-16 13:32:29 -0700

    Add puma config settings

--- config/puma.rb
+++ config/puma.rb
@@ -9,7 +9,7 @@ threads threads_count, threads_count
 
 # Specifies the `port` that Puma will listen on to receive requests, default is 3000.
 #
-port        ENV.fetch("PORT") { 3000 }
+port        ENV.fetch("PORT") { 9000 }
 
 # Specifies the `environment` that Puma will run in.
 #
@@ -44,4 +44,36 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 # end
 
 # Allow puma to be restarted by `rails restart` command.
-plugin :tmp_restart
+if ENV['RAILS_ENV'] != 'development'
+  plugin :tmp_restart
+
+  # Change to match your CPU core count
+  workers 2
+
+  # Min and Max threads per worker
+  threads 1, 6
+
+  app_dir = File.expand_path("../..", __FILE__)
+  shared_dir = "#{app_dir}/shared"
+
+  # Default to production
+  rails_env = ENV['RAILS_ENV'] || "production"
+  environment rails_env
+
+  # Set up socket location
+  bind "unix://#{shared_dir}/sockets/puma.sock"
+
+  # Logging
+  stdout_redirect "#{shared_dir}/log/puma.stdout.log", "#{shared_dir}/log/puma.stderr.log", true
+
+  # Set master PID and state locations
+  pidfile "#{shared_dir}/pids/puma.pid"
+  state_path "#{shared_dir}/pids/puma.state"
+  activate_control_app
+
+  on_worker_boot do
+    require "active_record"
+    ActiveRecord::Base.connection.disconnect! rescue ActiveRecord::ConnectionNotEstablished
+    ActiveRecord::Base.establish_connection(YAML.load_file("#{app_dir}/config/database.yml")[rails_env])
+  end
+end
