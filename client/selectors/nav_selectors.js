e4b9a5440e4ca2b2bdc8e8c2338745e9f4c4e7ef
Jin Chen <jinchen93@gmail.com>
2017-07-16 13:24:15 -0700

    Make Nav title compatible with Twitch display names

--- client/selectors/nav_selectors.js
+++ client/selectors/nav_selectors.js
@@ -1,25 +1,50 @@
+import { allTwitchChannels } from "../selectors/twitch_selectors";
+
 export const getName = (state, props) => {
   const { mode, id } = props.match.params;
   if (mode === "reddit") {
-    const hasSubreddits = Boolean(Object.keys(state.reddit.subreddits).length);
-    if (hasSubreddits) {
-      if (state.reddit.subreddits[id]) {
-        return state.reddit.subreddits[id].name;
-      } else {
-        return "All";
-      }
-    }
+    return getRedditTitle(state, id);
   } else if (mode === "youtube") {
-    const hasChannels = Boolean(Object.keys(state.youtube.channels).length);
-    if (hasChannels) {
-      return id ? state.youtube.channels[id].name : "Trending";
-    }
+    return getYoutubeTitle(state, id);
   } else if (mode === "twitch") {
-    const hasChannels = Boolean(Object.keys(state.twitch.channels).length);
-    if (hasChannels) {
-      return id ? state.twitch.channels[id].name : "Top Channels";
-    }
+    return getTwitchTitle(state, id);
   }
 
   return "";
+};
+
+const getRedditTitle = (state, id) => {
+  const hasSubreddits = Boolean(Object.keys(state.reddit.subreddits).length);
+  if (hasSubreddits) {
+    if (state.reddit.subreddits[id]) {
+      return state.reddit.subreddits[id].name;
+    } else {
+      return "All";
+    }
+  }
+};
+
+const getYoutubeTitle = (state, id) => {
+  const hasChannels = Boolean(Object.keys(state.youtube.channels).length);
+  if (hasChannels) {
+    return id ? state.youtube.channels[id].name : "Trending";
+  }
+};
+
+const getTwitchTitle = (state, id) => {
+  const hasChannels = Boolean(Object.keys(state.twitch.channels).length);
+  if (hasChannels) {
+    const channels = allTwitchChannels(state);
+    if (id) {
+      let channel = channels.find(channel => channel.idName === id);
+      if (channel) {
+        return channel.name;
+      } else {
+        channel = state.twitch.topStreams.find(channel => channel.name === id);
+        return channel ? channel.displayName : "";
+      }
+    } else {
+      return "Top Channels";
+    }
+  }
 };
