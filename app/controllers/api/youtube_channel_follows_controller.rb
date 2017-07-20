44f4dbe9dbcc7303ebe8f29622b506b8bbe50267
Jin Chen <jinchen93@gmail.com>
2017-07-20 10:54:38 -0700

    Remove name downcasing when adding youtube channels, youtube ID's are case sensitive

--- app/controllers/api/youtube_channel_follows_controller.rb
+++ app/controllers/api/youtube_channel_follows_controller.rb
@@ -1,24 +1,24 @@
 class Api::YoutubeChannelFollowsController < ApplicationController
   def create
-    channel_name = youtube_channel_follow_params[:name].downcase
+    channel_name = youtube_channel_follow_params[:name]
     @channel = YoutubeChannel.find_by(name: channel_name)
 
     # Check to see if channel exists, if not then create it=
     if @channel
-      create_follow(@channel)
+      create_follow
     else
       @channel = YoutubeChannel.new({ name: channel_name })
       if @channel.save
-        create_follow(@channel)
+        create_follow
       else
         render json: @channel.errors.full_messages, status: 422
       end
     end
   end
 
-  def create_follow(channel)
+  def create_follow
     follow = YoutubeChannelFollow.new({
-      youtube_channel_id: channel.id,
+      youtube_channel_id: @channel.id,
       user_id: current_user.id
     })
     if follow.save
