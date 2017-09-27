450996b206345d14dd36b935c25cdf8456157e5f
Jin Chen <jinchen93@gmail.com>
2017-09-26 21:23:08 -0700

    Add Flow typings for YoutubeActions

--- client/actions/youtube_actions.js
+++ client/actions/youtube_actions.js
@@ -1,3 +1,5 @@
+// @flow
+
 import * as YoutubeAPIUtil from "../utils/youtube_api_util";
 
 export const RECEIVE_YOUTUBE_FOLLOWS = "RECEIVE_YOUTUBE_FOLLOWS";
@@ -7,27 +9,27 @@ export const RECEIVE_YOUTUBE_CHANNEL = "RECEIVE_YOUTUBE_CHANNEL";
 export const DELETE_YOUTUBE_CHANNEL = "DELETE_YOUTUBE_CHANNEL";
 export const LOADING_YOUTUBE_VIDEOS = "LOADING_YOUTUBE_VIDEOS";
 
-export const receiveYoutubeFollows = channels => ({
+export const receiveYoutubeFollows = (channels: Array<Object>) => ({
   type: RECEIVE_YOUTUBE_FOLLOWS,
   channels,
 });
 
-export const receiveYoutubeVideos = videos => ({
+export const receiveYoutubeVideos = (videos: Array<Object>) => ({
   type: RECEIVE_YOUTUBE_VIDEOS,
   videos,
 });
 
-export const receiveYoutubeChannel = channel => ({
+export const receiveYoutubeChannel = (channel: Object) => ({
   type: RECEIVE_YOUTUBE_CHANNEL,
   channel,
 });
 
-export const deleteYoutubeChannel = id => ({
+export const deleteYoutubeChannel = (id: number) => ({
   type: DELETE_YOUTUBE_CHANNEL,
   id,
 });
 
-export const receiveYoutubeCurrentVideo = idx => ({
+export const receiveYoutubeCurrentVideo = (idx: number) => ({
   type: RECEIVE_YOUTUBE_CURRENT_VIDEO,
   idx,
 });
@@ -36,29 +38,29 @@ export const loadingYoutubeVideos = () => ({
   type: LOADING_YOUTUBE_VIDEOS,
 });
 
-export const fetchYoutubeFollows = () => dispatch =>
+export const fetchYoutubeFollows = () => (dispatch: Function) =>
   YoutubeAPIUtil.fetchYoutubeFollows().then(channels =>
     dispatch(receiveYoutubeFollows(channels))
   );
 
-export const fetchYoutubeVideos = id => dispatch => {
+export const fetchYoutubeVideos = (id: number) => (dispatch: Function) => {
   dispatch(loadingYoutubeVideos());
   return YoutubeAPIUtil.fetchYoutubeVideos(id).then(videos =>
     dispatch(receiveYoutubeVideos(videos))
   );
 };
 
-export const followYoutubeChannel = name => dispatch =>
+export const followYoutubeChannel = (name: string) => (dispatch: Function) =>
   YoutubeAPIUtil.followYoutubeChannel(name).then(channel =>
     dispatch(receiveYoutubeChannel(channel))
   );
 
-export const removeYoutubeChannel = id => dispatch =>
+export const removeYoutubeChannel = (id: number) => (dispatch: Function) =>
   YoutubeAPIUtil.removeYoutubeChannel(id).then(id =>
     dispatch(deleteYoutubeChannel(id))
   );
 
-export const fetchYoutubeMostPopular = () => dispatch => {
+export const fetchYoutubeMostPopular = () => (dispatch: Function) => {
   dispatch(loadingYoutubeVideos());
   return YoutubeAPIUtil.fetchYoutubeMostPopular().then(videos =>
     dispatch(receiveYoutubeVideos(videos))
