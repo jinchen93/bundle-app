c7f033a082a9446eba92020aabc989e5b90a2a0f
Jin Chen <jinchen93@gmail.com>
2018-09-16 10:20:34 -0700

    Run prettier and transform class properties

--- client/components/home/HomePage.jsx
+++ client/components/home/HomePage.jsx
@@ -1,9 +1,8 @@
 // @flow
-
-import React from "react";
-import NavContainer from "./NavContainer";
-import ListingContainer from "../listing/ListingContainer";
-import Media from "../media/Media";
+import React from 'react';
+import NavContainer from './NavContainer';
+import ListingContainer from '../listing/ListingContainer';
+import Media from '../media/Media';
 
 type Props = {
   mode: string,
@@ -22,53 +21,48 @@ class HomePage extends React.Component<Props, State> {
   handleVisibilityClick: Function;
   interval: ?number;
 
-  constructor() {
-    super();
-    this.fetchContent = this.fetchContent.bind(this);
-    this.handleVisibilityClick = this.handleVisibilityClick.bind(this);
-    this.state = { sideVisibility: true };
-  }
+  state = { sideVisibility: true };
 
   componentDidMount() {
     this.fetchContent();
   }
 
   componentDidUpdate() {
-    if (this.props.mode !== "twitch" && this.interval) {
+    if (this.props.mode !== 'twitch' && this.interval) {
       clearInterval(this.interval);
       this.interval = null;
     }
     this.fetchContent();
   }
 
-  fetchContent() {
+  fetchContent = () => {
     const mode = this.props.match.params.mode;
-    if (mode === "youtube") {
+    if (mode === 'youtube') {
       this.props.fetchYoutubeFollows();
-    } else if (mode === "reddit") {
+    } else if (mode === 'reddit') {
       this.props.fetchRedditFollows();
-    } else if (mode === "twitch") {
+    } else if (mode === 'twitch') {
       this.props.fetchTwitchFollows();
       this.interval = setInterval(() => {
         this.props.fetchTwitchFollows();
       }, 20000);
     }
-  }
+  };
 
-  handleVisibilityClick() {
+  handleVisibilityClick = () => {
     this.setState({ sideVisibility: !this.state.sideVisibility });
-  }
+  };
 
   render() {
     const { mode } = this.props.match.params;
     return (
-      <div className={"home-wrapper"}>
+      <div className={'home-wrapper'}>
         <NavContainer mode={mode} />
         <i
-          className={"ion-navicon-round"}
+          className={'ion-navicon-round'}
           onClick={this.handleVisibilityClick}
         />
-        <div className={"content-wrapper"}>
+        <div className={'content-wrapper'}>
           <ListingContainer visible={this.state.sideVisibility} mode={mode} />
           <Media mode={mode} />
         </div>
